6efc9e5c65be72263ef9dc828b8a7a790568e6f8
bfmhno3 <858446559@qq.com>
2026-08-14 17:20:57 +0800

    fix(system): correct shared NixOS configuration

--- modules/system.nix
+++ modules/system.nix
@@ -1,22 +1,34 @@
-{ pkgs, lib, username, ... }: {
+{
+  pkgs,
+  lib,
+  username,
+  ...
+}:
+{
 
   # Define a user content.
   # Don't forget to set a password with 'passwd'.
   users.users.${username} = {
     isNormalUser = true;
     description = username;
-    extraGroups = ["networkmanager" "wheel"];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
   };
 
   # give the users in this list the right to specify additional substituters via:
   #   1. `nixConfig.substituers` in `flake.nix`
   #   2. command line args `--options substituters http://xxx`
-  nix.settings.trusted-users = [username];
+  nix.settings.trusted-users = [ username ];
 
   # customise /etc/nix/nix.conf declaratively via `nix.settings`
   nix.settings = {
     # enable flakes globally
-    experimental-features = ["nix-command" "flakes"];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
 
     substituters = [
       # cache mirror located in China
@@ -52,7 +64,7 @@
   time.timeZone = "Asia/Shanghai";
 
   # Select internationalisation properties
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "zh_CN.UTF-8";
@@ -71,11 +83,100 @@
 
   fonts = {
     packages = with pkgs; [
+      # icon fonts
+      material-design-icons
 
+      # normal fonts
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+
+      # nerd fonts
+      # https://github.com/NixOS/nixpkgs/blob/nixos-unstable-small/pkgs/data/fonts/nerd-fonts/manifests/fonts.json
+      nerd-fonts.symbols-only # symbols icon only
+      nerd-fonts.fira-code
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.iosevka
     ];
+
+    # use fonts specified by user rather than default ones
+    enableDefaultPackages = false;
+
+    # user defined fonts
+    # the reason there's Noto Color Emoji everywhere is to override DejaVu's
+    # B&W emojis that would somtimes show instead of some Color emojis
+    fontconfig.defaultFonts = {
+      serif = [
+        "Noto Serif"
+        "Noto Color Emoji"
+      ];
+      sansSerif = [
+        "Noto Sans"
+        "Noto Color Emoji"
+      ];
+      monospace = [
+        "JetBrainsMono Nerd Font"
+        "Noto Color Emoji"
+      ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+
   };
 
-  programs.dconf.enable = true;
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPort = [ ... ];
+  # Or disable the firewall altogether
+  networking.firewall.enable = false;
 
+  # enable the OpenSSH daemon
+  services.openssh = {
+    enable = true;
+    settings = {
+      X11Forwarding = true;
+      PermitRootLogin = "no"; # disable root login
+      PasswordAuthentication = false; # disable password login
+    };
+    openFirewall = true;
+  };
 
+  # list packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    curl
+    git
+    sysstat
+    lm_sensors # for `sensors` command
+    fastfetch
+    thunar # XFCE file manager
+    nnn # terminal file manager
+  ];
+
+  # enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  services.power-profiles-daemon = {
+    enable = true;
+  };
+  security.polkit.enable = true;
+
+  services = {
+
+    geoclue2.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+
+      # If you want to use JACK application, uncomment this
+      jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      # media-session.enable = true;
+    };
+
+  };
 }
