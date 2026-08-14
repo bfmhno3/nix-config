e710b232d7fa607753511bd7aeafca4aeb26f088
bfmhno3 <858446559@qq.com>
2026-08-15 01:17:42 +0800

    feat(input): configure Rime Ice and Fcitx5 theme

--- home/fcitx5/default.nix
+++ home/fcitx5/default.nix
@@ -1,20 +1,88 @@
-{ pkgs, ... }: {
+{
+  config,
+  pkgs,
+  ...
+}:
+let
+  candlelightTheme = pkgs.stdenvNoCC.mkDerivation {
+    pname = "fcitx5-themes-candlelight";
+    version = "60aeadd";
+
+    src = pkgs.fetchzip {
+      url = "https://github.com/thep0y/fcitx5-themes-candlelight/archive/60aeaddfb3ecdb6a132e8da7569a6c442e6bb217.tar.gz";
+      hash = "sha256-YQ/2sveC57vQ36BjgnvPzObSAiAYmg1zK/TmEVqSvhY=";
+    };
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/share/fcitx5/themes
+      cp -r macOS-dark $out/share/fcitx5/themes/
+
+      runHook postInstall
+    '';
+  };
+  rimeIceDefaults = pkgs.writeTextDir "share/rime-data/default.custom.yaml" ''
+    patch:
+      schema_list:
+        - schema: rime_ice
+  '';
+  rimeIceSimplified = pkgs.writeTextDir "share/rime-data/rime_ice.custom.yaml" ''
+    patch:
+      switches/@2/reset: 0
+  '';
+in
+{
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
     fcitx5 = {
-      addons = [ pkgs.fcitx5-rime ];
+      addons = [
+        (pkgs.fcitx5-rime.override {
+          rimeDataPkgs = [
+            pkgs.rime-data
+            pkgs.rime-ice
+            rimeIceDefaults
+            rimeIceSimplified
+          ];
+        })
+        candlelightTheme
+      ];
       waylandFrontend = true;
-      settings.inputMethod = {
-        GroupOrder."0" = "Default";
-        "Groups/0" = {
-          Name = "Default";
-          "Default Layout" = "us";
-          DefaultIM = "rime";
+      settings = {
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "rime";
+          };
+          "Groups/0/Items/0".Name = "rime";
+          "Groups/0/Items/1".Name = "keyboard-us";
         };
-        "Groups/0/Items/0".Name = "keyboard-us";
-        "Groups/0/Items/1".Name = "rime";
+        addons.classicui.globalSection = {
+          "Vertical Candidate List" = "False";
+          PerScreenDPI = "True";
+          Theme = "macOS-dark";
+        };
       };
     };
+  };
+
+  home.file.".local/share/fcitx5/rime/.nix-rime-package" = {
+    text = "${config.i18n.inputMethod.package}\n";
+    onChange = ''
+      rime_dir="$HOME/.local/share/fcitx5/rime"
+      ${pkgs.coreutils}/bin/install -Dm644 \
+        "${rimeIceDefaults}/share/rime-data/default.custom.yaml" "$rime_dir/default.custom.yaml"
+      ${pkgs.coreutils}/bin/install -Dm644 \
+        "${rimeIceSimplified}/share/rime-data/rime_ice.custom.yaml" "$rime_dir/rime_ice.custom.yaml"
+      ${pkgs.librime}/bin/rime_deployer \
+        --build "$rime_dir" "${config.i18n.inputMethod.package}/share/rime-data" "$rime_dir/build"
+      (
+        cd "$rime_dir"
+        ${pkgs.librime}/bin/rime_deployer --set-active-schema rime_ice
+      )
+    '';
   };
 }
