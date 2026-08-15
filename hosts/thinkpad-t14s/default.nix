8a64c0b8b548377c1304e4bc08d7b048f449d8f0
bfmhno3 <858446559@qq.com>
2026-08-15 15:43:56 +0800

    fix(desktop): default to Hyprland session

--- hosts/thinkpad-t14s/default.nix
+++ hosts/thinkpad-t14s/default.nix
@@ -22,6 +22,7 @@
   };
 
   services.displayManager.sddm.enable = true;
+  services.displayManager.defaultSession = "hyprland-uwsm";
   services.desktopManager.plasma6.enable = true;
 
   services.fprintd.enable = true;
