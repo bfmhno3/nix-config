d8375279df073d1db3133abb713be71ddcd584d0
bfmhno3 <858446559@qq.com>
2026-08-15 16:49:13 +0800

    feat(user): install desktop and development tools

--- users/bfmhno3/nixos.nix
+++ users/bfmhno3/nixos.nix
@@ -6,7 +6,10 @@
 }:
 {
   users.users.${username} = {
-    extraGroups = [ "docker" ];
+    extraGroups = [
+      "docker"
+      "wireshark"
+    ];
     packages = with pkgs; [
       kdePackages.kate
       vscode
@@ -22,6 +25,27 @@
       easytier
       gimp
       texlive.combined.scheme-full
+
+      google-drive-ocamlfuse
+      telegram-desktop
+      zotero
+      obsidian
+      typora
+      kicad
+      android-tools
+      ffmpeg
+      ast-grep
+      drawio
+      freecad
+      gh
+      imhex
+      inkscape
+      neovim
+      rpi-imager
+      zeal
+      zed-editor
+      serial-studio
+      tio
     ];
   };
 
@@ -31,6 +55,14 @@
     inputs.helix.packages.${pkgs.stdenv.hostPlatform.system}.helix
     inputs.omp.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
+
+  programs = {
+    steam.enable = true;
+    wireshark = {
+      enable = true;
+      package = pkgs.wireshark;
+    };
+  };
 
   virtualisation.docker.enable = true;
   services.udev.packages = [ pkgs.openocd ];
