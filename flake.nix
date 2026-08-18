c2860b6aa4b1de1c70badc01635ca008d5520ba2
bfmhno3 <858446559@qq.com>
2026-08-18 19:15:48 +0800

    build(flake): add NixOS-WSL input

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     illogical-flake = {
       url = "github:soymou/illogical-flake";
       inputs.nixpkgs.follows = "nixpkgs";
