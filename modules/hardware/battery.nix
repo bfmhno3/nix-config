d4e593eb43a076daecfaad48642a085d7e96c045
bfmhno3 <858446559@qq.com>
2026-08-28 17:57:51 +0800

    feat(hardware): configure battery charge thresholds

--- modules/hardware/battery.nix
+++ modules/hardware/battery.nix
@@ -5,6 +5,13 @@ in
 {
   options.mySystem.hardware.battery.enable = lib.mkEnableOption "battery power management";
   config = lib.mkIf cfg.enable {
-    services.power-profiles-daemon.enable = true;
+    services.tlp = {
+      enable = true;
+      settings = {
+        START_CHARGE_THRESH_BAT0 = 75;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+      };
+    };
+    services.power-profiles-daemon.enable = false;
   };
 }
